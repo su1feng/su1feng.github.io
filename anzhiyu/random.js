f3188b9f913fb129934a74b7927dcbba3fdc39a9
su1feng <su1feng@outlook.com>
2025-12-14 15:49:26 +0800

    Site updated: 2025-12-14 15:49:26

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/12/07/hello-world/","2025/12/14/技术学习/","2025/12/14/刷题记录/","2025/12/15/个人随笔/"];function toRandomPost(){
+var posts=["2025/12/07/hello-world/","2025/12/15/个人随笔/","2025/12/14/刷题记录/","2025/12/14/技术学习/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
